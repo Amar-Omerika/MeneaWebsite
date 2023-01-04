899567561cd6a12f032853527b1e7813c23197af
NedimL00 <90788421+NedimL00@users.noreply.github.com>
2023-01-04 18:40:18 +0100

    First commit

--- src/components/Sublinks/SublinkPage.jsx
+++ src/components/Sublinks/SublinkPage.jsx
@@ -12,13 +12,13 @@ function SublinkPage({id, element}) {
       x.id===id &&
         <div key={id} className='font-customFont mt-[65px] w-full h-full'>
           <div className=' relative h-[300px] z-1'>
-            <img className='object-cover h-full w-full blur-[2px] brightness-50 shadow-2xl' src={x.bgImage} alt="engineering" />
+            <img className='object-cover h-full w-full blur-[2px] brightness-50 shadow-2xl' src={window.location.origin + `/images/${x.title}-Wallpaper.jpg`} alt="engineering" />
             <div className=' font-customFont absolute top-0 left-0 right-0 flex flex-col gap-9 justify-center items-center z-50 h-full text-6xl font-semibold text-white px-6 uppercase'>
               <p>{x.title}</p>
             </div>
           </div>
           <div className='h-full flex flex-col-reverse items-center w-full mt-6 p-6 space-y-6 laptop:flex-row '>
-          <img className='object-cover w-3/4 mt-16 laptop:w-1/2 laptop:px-6' src={x.image} alt='engineering' />
+          <img className='object-cover w-3/4 max-h-[400px] mt-16 laptop:w-1/2 laptop:px-6' src={window.location.origin + `/images/${x.title}.jpg`} alt='engineering' />
               <p className='laptop:w-1/2'>{x.content}</p>
           </div>
         </div>
