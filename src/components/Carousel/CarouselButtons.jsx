a2568ac1759c7a8228d2fae1354942a36492fcfe
Amar-Omerika <amar.omerika@edu.fit.ba>
2023-06-21 16:23:50 +0200

    Fixed problem with carousel buttons

--- src/components/Carousel/CarouselButtons.jsx
+++ src/components/Carousel/CarouselButtons.jsx
@@ -35,7 +35,7 @@ export default function CarouselButtons({ previousSlide, nextSlide }) {
 							}`}
 						></span>
 						<span
-							className={`relative w-full text-left transition-colors duration-200 ease-in-out ${
+							className={`relative w-full transition-colors duration-200 ease-in-out ${
 								isMobile
 									? "group-hover:text-indigo-600"
 									: "group-hover:text-white"
@@ -49,14 +49,13 @@ export default function CarouselButtons({ previousSlide, nextSlide }) {
 				<button onClick={nextSlide} type="button">
 					<a
 						href="#_"
-						className="relative inline-flex items-center justify-start py-3 pl-4 pr-4 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-4 hover:pr-6 bg-gray-50 group"
+						className="relative inline-flex items-center justify-start py-3 pl-4 pr-4 overflow-hidden font-semibold text-indigo-600 transition-all duration-150 ease-in-out rounded hover:pl-6 bg-gray-50 group"
 					>
 						<span
 							className={`absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out ${
-								isMobile ? "" : "bg-indigo-600"
+								isMobile ? "" : "bg-indigo-600 group-hover:h-full"
 							}`}
 						></span>
-
 						<span
 							className={`relative w-full transition-colors duration-200 ease-in-out ${
 								isMobile
